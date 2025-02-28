a281a2919fc0c5151ec3ebdab65bb8c519d84391
men_l <men_long@yahoo.com>
2025-02-28 11:36:14 -0500

    update

--- src/main/java/com/devops/coach/StartApplication.java
+++ src/main/java/com/devops/coach/StartApplication.java
@@ -6,26 +6,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
 @SpringBootApplication
 @Controller
 public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        // Get today's date
-        String todayDate = LocalDate.now().format(DateTimeFormatter.ofPattern("MMM d, yyyy"));
-
-        // Update messages with today's date and your name
-        model.addAttribute("title", "Welcome folks..We are learning Kubernetes Deployment using Helm, Jenkins Pipeline. Today's date is " + todayDate);
-        model.addAttribute("msg", "Hello All..This is Long Men. We are deploying a Spring Boot application into an EKS cluster using Helm + Jenkins Pipeline!!!!");
-
+        model.addAttribute("title", "Welcome folks..We are learning Kubernete Deployment using Helm, Jenkins Pipeline Today's date is Jan 18th, 2025");
+        model.addAttribute("msg", "Hello All..We are deploying springboot application into EKS cluster using Helm + Jenkins Pipeline!!!!");
         return "index";
     }
 
     public static void main(String[] args) {
         SpringApplication.run(StartApplication.class, args);
     }
+
 }
